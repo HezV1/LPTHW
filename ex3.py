97a5f246e49a98ae735c29b38f662fd9c5aad84a
hezakia1 <johndeereboy1997@gmail.com>
2014-04-07 19:25:01 -0500

    ex1-6

---
+++ ex3.py
@@ -0,0 +1,32 @@
+#Prints
+print "I will now count my chickens:"
+
+#25 plus 30 divided by 6
+print "Hens", 25 + 30 / 6
+#100 minus 25 times 3 remainder of 4
+print "Roosters", 100 - 25 * 3 % 4
+
+#prints
+print "Now I will count the eggs:"
+
+#3 plus 2 plus 1 minus 5 plus 4 remainder 2 minus 1 divided by 4 plus 6
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+#prints
+print "Is it true that 3 + 2 < 5 - 7?"
+
+#checks if 3 plus 2 is less than 5 minus 7
+print 3 + 2 < 5 - 7
+#print and does 3 plus 2
+print "What is 3 + 2?", 3 + 2
+#print and does 5 minus 7
+print "What is 5 - 7?", 5 - 7
+#print
+print "Oh, that's why it's False."
+
+#print 
+print "How about some more."
+
+print "is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "is it less or equal?", 5 <= -2
